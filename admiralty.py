a5acd1b766730d3441f50163c361a32dd0e79e7c
ahenryh <info@hauserhenry.com>
2025-10-14 14:40:06 +0200

    Add files via upload
    Exercice data portic

---
+++ admiralty.py
@@ -0,0 +1,20 @@
+import json
+import pandas
+import pprint
+url_to_read = "http://data.portic.fr/api/ports/?param=&shortenfields=false&both_to=false&date=1787"
+import requests
+resp = requests.get(url_to_read)
+#pretty_json = json.dumps(resp.json(), indent=1)
+data = pandas.DataFrame(resp.json())
+print(data)
+data.admiralty
+admiralty = [item for item in data.admiralty if item is not None]
+for item in admiralty:
+    if item not in set(admiralty):
+        set(admiralty).add(item)
+print(set(admiralty))
+print(len(set(admiralty)))
+if 'state_1789_fr' in data:
+    counts = data['state_1789_fr'].value_counts()
+    print(counts)
+counts.to_csv('counts.csv', header=True)
